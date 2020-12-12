bcb8c0d240fe274fdb43c059f445e8f26132fa23
mykereel <mgmartnz@icloud.com>
2020-12-11 17:58:36 -0700

    added const path to api routes and fixed delete functionality

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,7 @@
 //CONNECTS ROUTES TO NOTES DATA
 const notesData = require("../db/db.json");
 const fs = require("fs");
+const path = require("path");
 
 //ROUTING
 module.exports = (app) => {
@@ -29,5 +30,19 @@ module.exports = (app) => {
     notesData.push(req.body);
     writeToDB(notesData);
     res.json(req.body);
+  });
+
+  app.delete("/api/notes/:id", function (req, res) {
+    const id = req.params.id;
+    for (i = 0; i < notesData.length; i++) {
+      if (notesData[i].id === id) {
+        notesData.splice(i, 1);
+      }
+    }
+    fs.writeFileSync(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify({ notesData }, null, 2)
+    );
+    res.send(notesData);
   });
 };
